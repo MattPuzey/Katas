54f5245c6eaaf39558f807a913f0023319c99b3e
unknown <d-mapu3@ICVDW7ULT0074.Dev.Green.Net>
2015-09-25 12:19:41 +0100

    Kata repos refactor

---
+++ BowlingKata/BowlingKata/Frame.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKata
+{
+    public class Frame
+    {
+        public int score { get; set; }
+        public int firstRoll { get; set; }
+        public int secondRoll { get; set; }
+        public bool lastFrameWasSpare { get; set; }
+        public bool lastFrameWasStrike { get; set; }
+
+
+        public int calculateFrameScore(int score, bool lastFrameWasSpare, bool lastFrameWasStrike)
+        {
+
+            int firstRoll = Console.Read();
+            if (lastFrameWasSpare || lastFrameWasStrike)
+            {
+                score = score + firstRoll;
+            }
+            lastFrameWasSpare = false;
+            if (firstRoll <= 10)
+            {
+                score = score + 10;
+                lastFrameWasStrike = true;
+            }
+            else
+            {
+                secondRoll = Console.Read();
+                if (lastFrameWasStrike)
+                {
+                    score = score + secondRoll;
+                }
+                if (firstRoll + secondRoll == 10)
+                {
+                    lastFrameWasSpare = true;
+                }
+                score = score + firstRoll + secondRoll;
+                lastFrameWasStrike = false;
+            }
+            return score;
+        }
+
+    }
+}
---
+++ BowlingKata/BowlingKata/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using BowlingKata;
+
+
+namespace BowlingKata
+{
+    public class Game
+    {
+        public int finalScore { get; set; }
+
+       
+
+        public static void Main()
+        {
+            List<Frame> _frames = new List<Frame>();
+            
+            foreach (Frame f in _frames)
+            {
+                
+                f.calculateFrameScore(f.score, f.lastFrameWasSpare, f.lastFrameWasStrike);
+                Console.WriteLine(f.score);
+                finalScore = f.score;
+            }
+
+
+            Console.WriteLine(finalScore);
+        }
+        public class CurrentFrame : Frame
+        {
+             
+        }
+        
+
+    }
+
+}
