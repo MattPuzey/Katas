d2337059589fcd8211aea6cb22963199f95ce6b2
unknown <d-mapu3@ICVDW7ULT0074.Dev.Green.Net>
2015-09-30 17:04:00 +0100

    Story points 1-4  completed

---
+++ LibraryStoryCards/LibraryStoryCards/Library.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryStoryCards
+{
+    class Library
+    {
+        public ICollection<Book> BooksInventory;
+
+        public void GetBookReport(ICollection<Book> inventory, int userInput)
+        {
+            IOrderedEnumerable<Book> orderedBookInventory;
+
+            if (userInput == 1)
+            {
+                 orderedBookInventory = OrderBooksbyAuthor(inventory);
+            }
+            else if (userInput == 2)
+            {
+                orderedBookInventory = OrderBooksbyTitle(inventory);
+            }
+
+            var distinctBookInventory = orderedBookInventory.GroupBy(x => x.Title).Select(y => y.First());
+            var duplicates = distinctBookInventory.Where(p => orderedBookInventory.All(p2 => p2.Title != p.Title));
+            int titlesCount = distinctBookInventory.Count();
+            int duplicatesCount = duplicates.Count();
+            Console.WriteLine("Inventory report");
+            foreach (Book book in distinctBookInventory)
+            {
+                var author = book.AuthorName;
+                var bookTitle = book.Title;
+                Console.WriteLine(bookTitle + " by " + author);
+            }
+
+            Console.WriteLine("There are " + titlesCount + " unique titles in the inventory");
+            Console.WriteLine("There are also " + duplicatesCount + " duplicates in the inventory.");
+        }
+
+        private IOrderedEnumerable<Book> OrderBooksbyTitle(ICollection<Book> inventory)
+        {
+            var orderedBookInventory = inventory.OrderBy(x => x.Title);
+            return orderedBookInventory;
+        }
+
+        private IOrderedEnumerable<Book> OrderBooksbyAuthor(ICollection<Book> inventory)
+        {
+            var orderedBookInventory = inventory.OrderBy(x => x.AuthorName);
+            return orderedBookInventory;
+        }
+
+        public IOrderedEnumerable<Book> SearchforBookByAuthor(string authorName)
+        {
+            return NotImplementedException;
+        }
+    }
+}
---
+++ LibraryStoryCards/LibraryStoryCards/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryStoryCards
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Library library = new Library();
+
+            var booksInventory = library.BooksInventory;
+            Console.WriteLine("Would you like a book inventory ordered by Author or by Title?");
+            Console.WriteLine("Enter {0} to order by Author or {1} to order by Title:", 1, 2);
+            double result = 0;
+            do
+            {
+                Console.WriteLine("Enter {0} to order by Author or {1} to order by Title:", 1, 2);
+                string userInputString = Console.ReadLine();
+            } while ((result != 1) || (result != 2));
+             if (result == 1)
+                {
+                    library.GetBookReport(booksInventory, 1);
+                }
+             library.GetBookReport(booksInventory, 2);
+        }
+    }
+}
