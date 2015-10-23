77417bf2490048678e917532414260cadd6aac38
MattPuzey <mpuzey1@outlook.com>
2015-10-23 09:57:32 +0100

    FizzBuzz TDD

---
+++ FizzBuzz/23.10.15/FizzBuzzKata/FizzBuzz.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzKata
+{
+    internal class FizzBuzz
+    {
+        public bool IsDivisisbleByThree(int input)
+        {
+            return (input%3) == 0;
+        }
+
+        public bool IsDivisisbleByFive(int input)
+        {
+            return (input%5) == 0;
+        }
+
+        public string GetTextForNumber(int input)
+        {
+            if (IsDivisisbleByThree(input) & IsDivisisbleByFive(input))
+            {
+                return "FizzBuzz";
+            }
+            if (IsDivisisbleByFive(input))
+            {
+                return "Buzz";
+            }
+            if (IsDivisisbleByThree(input))
+            {
+                return "Fizz";
+            }
+            else
+            {
+               return input.ToString();
+            }
+        }
+
+        public string DoTheWholeThing(int rangeMaximum)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= rangeMaximum; ++i)
+            {
+                sb.AppendFormat("{0} ", GetTextForNumber(i));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ FizzBuzz/23.10.15/FizzBuzzKata/FizzBuzzTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzzKata
+{
+    [TestFixture]
+    internal class FizzBuzzTets
+    {
+        private FizzBuzz fb = new FizzBuzz();
+
+        [Test]
+        public void ThreeIsDivisibleByThreeTest()
+        {
+            bool divisible = fb.IsDivisisbleByThree(3);
+
+            Assert.That(divisible, Is.True);
+        }
+
+        [Test]
+        public void FiveIsDivisibleByFiveTest()
+        {
+            bool divisible = fb.IsDivisisbleByFive(5);
+
+            Assert.That(divisible, Is.True);
+        }
+
+        [Test]
+        public void TextForDivisibleByThreeIsFizzTest()
+        {
+            string text = fb.GetTextForNumber(3);
+
+            Assert.That(text, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void TextForDivisibleByFiveIsBuzzTest()
+        {
+            string text = fb.GetTextForNumber(5);
+
+            Assert.That(text, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void TextForDivisbleByThreeAndFiveIsFizzBuzz()
+        {
+            string text = fb.GetTextForNumber(15);
+
+            Assert.That(text, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void TextForNotDivisibleByThreeOrFiveIsNumber()
+        {
+            string text = fb.GetTextForNumber(4);
+
+            Assert.That(text, Is.EqualTo("4"));
+        }
+
+        [Test]
+        public void CanDoTheWholeThingTest()
+        {
+            string allOfIt = fb.DoTheWholeThing(15);
+
+            Assert.That(
+                allOfIt, 
+                Is.EqualTo("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz "));
+        }
+    }
+}
